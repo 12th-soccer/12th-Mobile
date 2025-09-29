4dd636b255f0a2a699e96e27aff8df9129553fca
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-09-29 13:03:41 +0900

    feat :: 디자인 시스템 추가 (#1)
    폰트, 색상, 텍스트스타일 정의

---
+++ lib/constants/color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/painting.dart';
+
+abstract final class TwelfthColor {
+  static const Color main = Color(0xFFC4C7D4);
+  static const Color background = Color(0xFF1A1A1A);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color gray500 = Color(0xFF939393);
+  static const Color gray900 = Color(0xFF414141);
+  static const Color gray950 = Color(0xFF1C1C1C);
+
+  static const List<Color> silverGradient = [
+    Color(0xFFFFFFFF),
+    Color(0xFF8C92AC)
+  ];
+}
