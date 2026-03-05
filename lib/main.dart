86e27246623c1ba4fd14abbf33347176d2103af0
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-05 22:28:33 +0900

    feat: main 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:material_symbols_icons/symbols.dart';
-import 'package:twelfth_mobile/common/components/button/elevated_button.dart';
 import 'package:twelfth_mobile/constants/color.dart';
-import 'package:twelfth_mobile/constants/twelfth_assets.dart';
-import 'package:twelfth_mobile/views/main_app.dart';
+import 'package:twelfth_mobile/views/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,51 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: TwelfthColor.background),
-      // home: TwelfthMainApp(),
-      // home: TwelfthSplashView(),
-      home: Scaffold(
-        body: Center(
-          /*child: Column(
-            children: [
-              const SizedBox(height: 50),
-              TwelfthElevatedButton(
-                backgroundColor: TwelfthColor.yellow,
-                onPressed: () {
-                  print('단색 버튼 활성 클릭');
-                },
-                textColor: TwelfthColor.black,
-                imgPath: TwelfthAssets.kakao,
-                child: const Text('Kakao 로그인'),
-              ),
-              const SizedBox(height: 20),
-              TwelfthElevatedButton(
-                backgroundColor: TwelfthColor.white,
-                onPressed: null,
-                textColor: TwelfthColor.black,
-                imgPath: TwelfthAssets.kakao,
-                child: const Text('Kakao 로그인 (비활성)'),
-              ),
-            ],
-          ),*/
-          child: TwelfthElevatedButton(
-            imgPath: TwelfthAssets.kakao,
-            child: Text('kakao 로그인 ㄱ?'),
-          ),
-          /*child: TwelfthElevatedButton(
-            imgPath: TwelfthAssets.kakao,
-            gradient: TwelfthGradient.horizontal(TwelfthColor.silverGradient),
-            onPressed: () {
-              print('눌림!!!');
-            },
-            child: const Text('kakao 로그인 ㄱ?'),
-          ),*/
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: CustomColor.background),
+      home: const TwelfthSplashView(),
     );
   }
 }
