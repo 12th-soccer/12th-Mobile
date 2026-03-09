373e2de61074e50eff3f553a544c5b4a7ff8fa12
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-09 16:17:49 +0900

    feat: 앱바 추가

---
+++ lib/common/components/app_bar/twelfth_app_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:twelfth_mobile/constants/color.dart';
+import 'package:twelfth_mobile/constants/text_style.dart';
+
+class TwelfthAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String? title;
+  final bool showBackButton;
+  final List<Widget>? actions;
+  final Widget? leading;
+  final Color backgroundColor;
+
+  const TwelfthAppBar({
+    super.key,
+    this.title,
+    this.showBackButton = true,
+    this.actions,
+    this.leading,
+    this.backgroundColor = CustomColor.background,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: backgroundColor,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      centerTitle: true,
+      leading: showBackButton
+          ? (leading ??
+                IconButton(
+                  icon: const Icon(
+                    Symbols.arrow_back_ios_new,
+                    color: CustomColor.white,
+                    size: 20,
+                  ),
+                  onPressed: () => context.pop(),
+                ))
+          : leading,
+      title: title != null
+          ? Text(title!, style: CustomTextStyle.heading2)
+          : null,
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
