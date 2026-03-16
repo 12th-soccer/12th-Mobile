cbc45cd4b3991945ab572a4ec460d3aa8b8a596a
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-16 15:09:50 +0900

    fix: 상태관리 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twelfth_mobile/constants/color.dart';
 import 'package:twelfth_mobile/core/router/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
