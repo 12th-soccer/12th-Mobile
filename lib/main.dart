e93fc8195fdf4849ab655b818a01d9e357826ec1
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-04-13 23:03:25 +0900

    feat: firebase 설정 오류 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
+import 'dart:developer' as developer;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twelfth_mobile/constants/color.dart';
 import 'package:twelfth_mobile/core/router/router.dart';
+import 'package:twelfth_mobile/firebase_options.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  developer.log(
+    '[FCM] 백그라운드 메시지 수신\n'
+    '  title: ${message.notification?.title}\n'
+    '  body: ${message.notification?.body}',
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  runApp(const ProviderScope(child: TwelfthApp()));
+}
+
+class TwelfthApp extends StatelessWidget {
+  const TwelfthApp({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/views/main_app.dart
+++ lib/views/main_app.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:twelfth_mobile/common/components/bottom_nav/twelfth_bottom_nav_bar.dart';
 import 'package:twelfth_mobile/constants/color.dart';
+import 'package:twelfth_mobile/core/services/fcm_service.dart';
 
-class TwelfthMainApp extends StatelessWidget {
+class TwelfthMainApp extends ConsumerStatefulWidget {
   final StatefulNavigationShell navigationShell;
 
   const TwelfthMainApp({super.key, required this.navigationShell});
 
   @override
+  ConsumerState<TwelfthMainApp> createState() => _TwelfthMainAppState();
+}
+
+class _TwelfthMainAppState extends ConsumerState<TwelfthMainApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FcmService.initialize(ref);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColor.background,
-      body: navigationShell,
+      body: widget.navigationShell,
       bottomNavigationBar: TwelfthBottomNavBar(
-        currentIndex: navigationShell.currentIndex,
+        currentIndex: widget.navigationShell.currentIndex,
         onTap: (index) {
-          navigationShell.goBranch(index, initialLocation: true);
+          widget.navigationShell.goBranch(index, initialLocation: true);
         },
       ),
     );
