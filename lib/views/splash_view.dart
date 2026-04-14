9f1a2dda52f67b60038ce68fd589a4a05afa5227
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-04-14 12:21:13 +0900

    feat: 토큰 여부에 따른 스플래시 뷰 분기

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -4,13 +4,48 @@ import 'package:go_router/go_router.dart';
 import 'package:twelfth_mobile/common/components/button/elevated_button.dart';
 import 'package:twelfth_mobile/constants/color.dart';
 import 'package:twelfth_mobile/constants/twelfth_assets.dart';
+import 'package:twelfth_mobile/core/network/token_storage.dart';
 import 'package:twelfth_mobile/core/router/router_paths.dart';
 
-class TwelfthSplashView extends StatelessWidget {
+class TwelfthSplashView extends StatefulWidget {
   const TwelfthSplashView({super.key});
 
   @override
+  State<TwelfthSplashView> createState() => _TwelfthSplashViewState();
+}
+
+class _TwelfthSplashViewState extends State<TwelfthSplashView> {
+  bool _checked = false;
+  bool _hasToken = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkToken();
+  }
+
+  Future<void> _checkToken() async {
+    final hasToken = await TokenStorage.instance.hasToken();
+    if (!mounted) return;
+    if (hasToken) {
+      context.go(AppRoutes.ranking);
+    } else {
+      setState(() {
+        _checked = true;
+        _hasToken = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (!_checked || _hasToken) {
+      return Scaffold(
+        backgroundColor: CustomColor.background,
+        body: Center(child: SvgPicture.asset(TwelfthAssets.logo)),
+      );
+    }
+
     return Scaffold(
       backgroundColor: CustomColor.background,
       body: SafeArea(
