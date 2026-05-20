09118a71a2d6a79512cf522d4b0b0f408e0afa8b
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-05-20 14:15:13 +0900

    feat: 스낵바 성공 메시지 추가

--- lib/core/extensions/snackbar_extension.dart
+++ lib/core/extensions/snackbar_extension.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:twelfth_mobile/common/components/snack_bar/custom_snack_bar.dart';
+import 'package:twelfth_mobile/core/constants/color.dart';
 
 extension SnackBarExtension on BuildContext {
   void showErrorSnackBar(String message) {
     ScaffoldMessenger.of(this).showSnackBar(
       CustomSnackBar.error(message),
+    );
+  }
+
+  void showSuccessSnackBar(String message) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: CustomColor.green,
+        behavior: SnackBarBehavior.floating,
+      ),
     );
   }
 }
